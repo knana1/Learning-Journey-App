57cce9ec32c0dbd7481b5da30c5bd5c4a88768d8
knana <knana.alshabibi@gmail.com>
2024-11-04 15:03:55 +0300

    Initial Commit

---
+++ Learning Journey App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning Journey App
+//
+//  Created by Knana Adel Alshabibi on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning Journey App/Learning_Journey_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Journey_AppApp.swift
+//  Learning Journey App
+//
+//  Created by Knana Adel Alshabibi on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Journey_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
